7f14ddaf0b24d80e25fced1fe694a2c17d1c2ff9
Nicolas Filippelli <Nicolas.Filippelli@coupa.com>
2021-09-28 17:33:36 +0200

    setup repo

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,16 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const githubToken = core.getInput('GITHUB_TOKEN', {required: true})
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    github.getOctokit(githubToken)
+    core.info(`GITHUB_EVENT_NAME=${process.env.GITHUB_EVENT_NAME}`)
+    core.info(`GITHUB context action=${github.context.payload.action}`)
   } catch (error) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     core.setFailed(error.message)
   }
 }
